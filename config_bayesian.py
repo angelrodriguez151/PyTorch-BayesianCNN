02ef5fb91ca3344864aa25fe7083ee35aa95751c
angelrodriguez151 <82665455+angelrodriguez151@users.noreply.github.com>
2022-04-09 12:11:33 -0500

    siiiiiiiiiii}

--- config_bayesian.py
+++ config_bayesian.py
@@ -8,7 +8,7 @@ priors={
     'posterior_rho_initial': (-5, 0.1),  # (mean, std) normal_
 }
 
-n_epochs = 20
+n_epochs = 40
 lr_start = 0.001
 num_workers = 2
 valid_size = 0.2
--- config_frequentist.py
+++ config_frequentist.py
@@ -1,5 +1,5 @@
 ############### Configuration file for Frequentist ###############
-n_epochs = 20
+n_epochs = 40
 lr = 0.001
 num_workers = 2
 valid_size = 0.2
