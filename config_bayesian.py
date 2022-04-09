3cfdfb42c7f2568b760ec2fd68ac62fca2b987b0
angelrodriguez151 <82665455+angelrodriguez151@users.noreply.github.com>
2022-04-09 14:00:10 -0500

    Update config_bayesian.py

--- config_bayesian.py
+++ config_bayesian.py
@@ -4,8 +4,8 @@ activation_type = 'softplus'  # 'softplus' or 'relu'
 priors={
     'prior_mu': 0,
     'prior_sigma': 0.02,
-    'posterior_mu_initial': (0, 0.1),  # (mean, std) normal_
-    'posterior_rho_initial': (-5, 0.1),  # (mean, std) normal_
+    'posterior_mu_initial': (0, 0.02),  # (mean, std) normal_
+    'posterior_rho_initial': (-5, 0.02),  # (mean, std) normal_
 }
 
 n_epochs = 40
