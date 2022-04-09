065883bfa1359ddddbd7d3514d0442025262e224
angelrodriguez151 <82665455+angelrodriguez151@users.noreply.github.com>
2022-04-09 09:23:08 -0500

    Update config_frequentist.py

--- config_frequentist.py
+++ config_frequentist.py
@@ -1,6 +1,6 @@
 ############### Configuration file for Frequentist ###############
-n_epochs = 40
-lr = 0.005
+n_epochs = 20
+lr = 0.001
 num_workers = 2
 valid_size = 0.2
-batch_size = 64
+batch_size = 32
