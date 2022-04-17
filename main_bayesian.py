3cc9d249787648abfed388f3a4ef78e6e523d60a
angelrodriguez151 <82665455+angelrodriguez151@users.noreply.github.com>
2022-04-17 15:50:18 -0500

    Update main_bayesian.py

--- main_bayesian.py
+++ main_bayesian.py
@@ -80,7 +80,7 @@ def validate_model(net, criterion, validloader, num_ens=1, beta_type=0.1, epoch=
         beta = metrics.get_beta(i-1, len(validloader), beta_type, epoch, num_epochs)
         valid_loss += criterion(log_outputs, labels, kl, beta).item()
         accs.append(metrics.acc(log_outputs, labels))
-        cm = confusion_matrix(y_pred = net_out[:,0].to(torch.device('cpu')).detach().numpy().astype('float32'), y_true = labels.to(torch.device('cpu')).detach().numpy())
+        cm = confusion_matrix(ypred= ( net_out[:,0]>0.92).to(torch.device('cpu')).detach().numpy().astype('float32'), y_true = labels.to(torch.device('cpu')).detach().numpy())
 
 
     return valid_loss/len(validloader), np.mean(accs), cm
