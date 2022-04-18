3f510a532c372e721b65f3d57635a02e2e45efc6
angelrodriguez151 <82665455+angelrodriguez151@users.noreply.github.com>
2022-04-18 13:28:42 -0500

    Update config_bayesian.py

--- config_bayesian.py
+++ config_bayesian.py
@@ -3,7 +3,7 @@ layer_type = 'lrt' # 'bbb' or 'lrt'
 activation_type = 'softplus'  # 'softplus' or 'relu'
 priors={
     'prior_mu': 0,
-    'prior_sigma': 0.2,
+    'prior_sigma': 0.1,
     'posterior_mu_initial': (0, 0.4),  # (mean, std) normal_
     'posterior_rho_initial': (-5, 0.4),  # (mean, std) normal_
 }
