1044fa77eafe48b7ab079b9d90981ed9305ebedd
angelrodriguez151 <82665455+angelrodriguez151@users.noreply.github.com>
2022-04-19 08:24:03 -0500

    Update nodropout.py

--- models/NonBayesianModels/nodropout.py
+++ models/NonBayesianModels/nodropout.py
@@ -18,17 +18,17 @@ class nodropout(nn.Module):
         super(nodropout, self).__init__()
         self.features = nn.Sequential(
             
-            nn.Conv2d(inputs, 8, 3),
+            nn.Conv2d(inputs, 16, 3),
             nn.Softplus(),
-            nn.MaxPool2d(6,6),
+            nn.MaxPool2d(3,3),
             
             
         )
         self.classifier = nn.Sequential(
             nn.Flatten(1),
-            nn.Linear(800, 128),
+            nn.Linear(6400, 1024),
             nn.Softplus(),
-            nn.Linear(128, outputs),
+            nn.Linear(1024, outputs),
     
         )
 
