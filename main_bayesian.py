5c6b4d42460f7e2cba8a01eee2c5fd7d5a3d5dcf
angelrodriguez151 <82665455+angelrodriguez151@users.noreply.github.com>
2022-06-13 22:02:06 -0500

    Update main_bayesian.py

--- main_bayesian.py
+++ main_bayesian.py
@@ -50,7 +50,7 @@ def train_model(net, optimizer, criterion, trainloader, num_ens=1, beta_type=0.1
         kl = kl / num_ens
         kl_list.append(kl.item())
         print("ok")
-        log_outputs = outputs
+        log_outputs = outputs.reshape(-1)
         print("ok2")
         beta = metrics.get_beta(i-1, len(trainloader), beta_type, epoch, num_epochs)
         print("ok3")
