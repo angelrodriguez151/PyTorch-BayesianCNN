ee4f90e255148a162faf17c7a40133e3f0d29715
angelrodriguez151 <82665455+angelrodriguez151@users.noreply.github.com>
2022-06-13 22:11:40 -0500

    Update main_bayesian.py

--- main_bayesian.py
+++ main_bayesian.py
@@ -45,11 +45,11 @@ def train_model(net, optimizer, criterion, trainloader, num_ens=1, beta_type=0.1
         for j in range(num_ens):
             net_out, _kl = net(inputs)
             kl += _kl
-            outputs[:, j] = F.sigmoid(net_out.reshape(-1))
+            outputs[:, j] = torch.sigmoid(net_out.reshape(-2))
         
         kl = kl / num_ens
         kl_list.append(kl.item())
-        log_outputs = outputs.reshape(-1)
+        log_outputs = outputs
         beta = metrics.get_beta(i-1, len(trainloader), beta_type, epoch, num_epochs)
         loss = criterion(log_outputs, labels, kl, beta)
         loss.backward()
@@ -66,14 +66,14 @@ def validate_model(net, criterion, validloader, num_ens=1, beta_type=0.1, epoch=
     accs = []
     for i, (inputs, labels) in enumerate(validloader):
         inputs, labels = inputs.to(device), labels.to(device).float()
-        outputs = torch.zeros(inputs.shape[0], net.num_classes, num_ens).to(device)
+        outputs = torch.zeros(inputs.shape[0], num_ens).to(device)
         kl = 0.0
         for j in range(num_ens):
             net_out, _kl = net(inputs)
             kl += _kl
-            outputs[:, :, j] = F.sigmoid(net_out.reshape(-1)).data
+            outputs[:, j] = torch.sigmoid(net_out.reshape(-2)).data
 
-        log_outputs = outputs.reshape(-1)
+        log_outputs = outputs
         beta = metrics.get_beta(i-1, len(validloader), beta_type, epoch, num_epochs)
         valid_loss += criterion(log_outputs, labels, kl, beta).item()
         accs.append(metrics.acc(log_outputs, labels))
@@ -91,14 +91,14 @@ def testing(net,  testloader, num_ens=1, beta_type=0.1, epoch=None, num_epochs=N
     sens=[]
     for i, (inputs, labels) in enumerate(testloader):
         inputs, labels = inputs.to(device), labels.to(device).float()
-        outputs = torch.zeros(inputs.shape[0], net.num_classes, num_ens).to(device)
+        outputs = torch.zeros(inputs.shape[0], num_ens).to(device)
         kl = 0.0
         for j in range(num_ens):
             net_out, _kl = net(inputs)
             kl += _kl
-            outputs[:, :, j] = F.log_softmax(net_out, dim=1).data
+            outputs[:, j] = torch.log_softmax(net_out, dim=1).data
 
-        log_outputs = utils.logmeanexp(outputs, dim=2)
+        log_outputs = outputs.reshape(-1)
         beta = metrics.get_beta(i-1, len(testloader), beta_type, epoch, num_epochs)
         accs.append(metrics.acc(log_outputs, labels))
         spec.append(metrics.specificity(log_outputs, labels))
