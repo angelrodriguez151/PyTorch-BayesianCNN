99d6dbcb3db3c685760c02538d6b796d39f56a6c
angelrodriguez151 <82665455+angelrodriguez151@users.noreply.github.com>
2022-06-14 22:09:17 -0500

    Update config_bayesian.py

--- config_bayesian.py
+++ config_bayesian.py
@@ -3,16 +3,16 @@ layer_type = 'lrt' # 'bbb' or 'lrt'
 activation_type = 'softplus'  # 'softplus' or 'relu'
 priors={
     'prior_mu': 0,
-    'prior_sigma': 0.1,
-    'posterior_mu_initial': (0, 0.1),  # (mean, std) normal_
-    'posterior_rho_initial': (-5, 0.1),  # (mean, std) normal_
+    'prior_sigma': 0.01,
+    'posterior_mu_initial': (0, 0.5),  # (mean, std) normal_
+    'posterior_rho_initial': (-5, 0.5),  # (mean, std) normal_
 }
 
 n_epochs = 15
 lr_start = 0.001
 num_workers = 2
 valid_size = 0.3
-batch_size = 16
+batch_size = 128
 train_ens = 1
 valid_ens = 1
 beta_type = 0.1 # 'Blundell', 'Standard', etc. Use float for const value
