298c037e77fae3b180ddba32b4d2b68aa3935e7e
angelrodriguez151 <82665455+angelrodriguez151@users.noreply.github.com>
2022-07-11 07:38:23 -0500

    Update __init__.py

--- layers/__init__.py
+++ layers/__init__.py
@@ -1,7 +1,9 @@
 from .BBB.BBBLinear import BBBLinear as BBB_Linear
 from .BBB.BBBConv import BBBConv2d as BBB_Conv2d
+from .BBB.BBBConv1d import BBBConv1d as BBB_Conv1d
 
 from .BBB_LRT.BBBLinear import BBBLinear as BBB_LRT_Linear
 from .BBB_LRT.BBBConv import BBBConv2d as BBB_LRT_Conv2d
+from .BBB_LRT.BBBConv1d import BBBConv1d as BBB_LRT_Conv1d
 
 from .misc import FlattenLayer, ModuleWrapper
