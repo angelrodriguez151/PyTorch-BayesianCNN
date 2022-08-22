3359773b029d380a2dca8c0689dab3c56d2a976e
RODRIGUEZ Angel E. TENARIS <AERODRIGUEZ@tenaris.com>
2022-08-21 21:02:41 -0500

    agregar modelo lineal

--- models/BayesianModels/Bayesian1DConv.py
+++ models/BayesianModels/Bayesian1DConv.py
@@ -40,4 +40,36 @@ class BBBConv1(ModuleWrapper):
         self.fc1 = BBBLinear(68, 32, bias=True, priors=self.priors)
         self.act4 = self.act()
         self.fc2 = BBBLinear(32, outputs, bias=True, priors=self.priors)
+
+
+class BBBLinear(ModuleWrapper):
+    """
+
+    Simple Neural Network having 3 Convolution
+    and 3 FC layers with Bayesian layers.
+    """
+    def __init__(self, outputs, inputs, priors, layer_type='lrt', activation_type='softplus'):
+        super(BBBLinear, self).__init__()
+
+        self.num_classes = outputs
+        self.layer_type = layer_type
+        self.priors = priors
+
+        if layer_type=='lrt':
+            BBBLinear = BBB_LRT_Linear
+        elif layer_type=='bbb':
+            BBBLinear = BBB_Linear
+        else:
+            raise ValueError("Undefined layer_type")
+        
+        if activation_type=='softplus':
+            self.act = nn.Softplus
+        elif activation_type=='relu':
+            self.act = nn.ReLU
+        else:
+            raise ValueError("Only softplus or relu supported")
+
+        self.fc1 = BBBLinear(44, 64, bias=True, priors=self.priors)
+        self.act1 = self.act()
+        self.fc2 = BBBLinear(64, outputs, bias=True, priors=self.priors)
         
