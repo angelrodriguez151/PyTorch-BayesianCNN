bd63cefe09e6b99b6707772a5ad82e6668aa333d
RODRIGUEZ Angel E. TENARIS <AERODRIGUEZ@tenaris.com>
2022-08-23 17:15:44 -0500

    16 epocas

--- config_bayesian.py
+++ config_bayesian.py
@@ -3,8 +3,8 @@ layer_type = 'lrt' # 'bbb' or 'lrt'
 activation_type = 'softplus'  # 'softplus' or 'relu'
 priors={
     'prior_mu': 0,
-    'prior_sigma': 0.5,
-    'posterior_mu_initial': (0, 5),  # (mean, std) normal_
+    'prior_sigma': 0.05,
+    'posterior_mu_initial': (0, 0.5),  # (mean, std) normal_
     'posterior_rho_initial': (-5, 0.5),  # (mean, std) normal_
 }
 
@@ -12,7 +12,7 @@ n_epochs = 200
 lr_start = 0.0001
 num_workers = 2
 valid_size = 0.2
-batch_size = 8
+batch_size = 16
 train_ens = 1
 valid_ens = 1
 beta_type = 0.1 # 'Blundell', 'Standard', etc. Use float for const value
